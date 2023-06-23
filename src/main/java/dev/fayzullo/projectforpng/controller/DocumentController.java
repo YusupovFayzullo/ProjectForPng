54e984da6c2a6541369deac9808e29fd11b8d863
user <yusupovfayzullo100@gmail.com>
2023-06-23 22:55:28 +0500

    Bismillah

--- src/main/java/dev/fayzullo/projectforpng/controller/DocumentController.java
+++ src/main/java/dev/fayzullo/projectforpng/controller/DocumentController.java
@@ -43,7 +43,6 @@ public class DocumentController {
 
     @GetMapping(value = "/open/{fileName}")
     public ResponseEntity<FileUrlResource>  openFile(@PathVariable String fileName) throws MalformedURLException {
-        System.out.println();
         Document document= documentService.getDocumentBy(fileName);
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(document.getMimeType()))
